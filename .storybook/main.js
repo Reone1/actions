9e57fc32f229a4232b3001ab103bffcd68a0a258
jae1choi <jw.choi@mxncommerce.com>
2021-11-04 14:44:14 +0900

    Feat : :sparkles: : Add StoryBook

---
+++ .storybook/main.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const tsConfigPathPlugin = require('tsconfig-paths-webpack-plugin');
+const toPath = (filePath) => path.join(process.cwd(), filePath);
+
+module.exports = {
+  webpackFinal: async (config) => {
+    config.resolve.plugins.push(new tsConfigPathPlugin({}));
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          '@emotion/core': toPath('node_modules/@emotion/react'),
+          'emotion-theming': toPath('node_modules/@emotion/react'),
+        },
+      },
+    };
+  },
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
+  },
+  stories: ['../src/component/**/*.spec.tsx'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-docs'],
+};
