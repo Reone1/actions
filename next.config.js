681c17fdb10e55d13a75a8b37e15c44a0c27cfff
jae1choi <jw.choi@mxncommerce.com>
2021-11-08 15:18:38 +0900

    refactor::hammer:: modify peer dependency & next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  swcMinify: false,
   webpack: (config, { dev }) => {
     if (dev) return { ...config, cache: false };
     return config;
